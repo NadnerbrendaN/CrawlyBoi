d0e0d598396c2f1f052bcfcaaa97d1f433e029d6
NadnerbrendaN <135467483+NadnerbrendaN@users.noreply.github.com>
2024-04-13 11:54:44 -0400

    Added control
    Made it so you can press escape to stop the crawler, and it opens the most recently scraped tab in your web browser

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 from requests_html import HTMLSession
+from pynput import keyboard
+import webbrowser
 
 visited_links = []
 keyword = 'https://en.wikipedia.org/wiki'
+doCrawl = True
+
+def on_press(key):
+    if key == keyboard.Key.esc:
+        global doCrawl 
+        doCrawl = False
+        return False
 
 def crawl(url,session):
     r = session.get(url)
     for link in r.html.absolute_links:
-        if link not in visited_links and keyword in link:
+        if doCrawl is False:
+            break
+        elif link not in visited_links and keyword in link:
             visited_links.append(link)
             print(link)
             crawl(link,session)
 
 def main(url):
+    listener = keyboard.Listener(on_press=on_press)
+    listener.start()
+    #listener.join()
     session = HTMLSession()
     crawl(url,session)
+    webbrowser.open(visited_links[-1])
 
 if __name__=='__main__':
     url = 'https://en.wikipedia.org/wiki/Special:Random'

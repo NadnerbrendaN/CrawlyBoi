f673716ab5197469fdc0f9558c79dfca8c65dcf2
NadnerbrendaN <135467483+NadnerbrendaN@users.noreply.github.com>
2024-04-14 11:50:39 -0400

    Commented Code

--- main.py
+++ main.py
@@ -1,35 +1,33 @@
-from requests_html import HTMLSession
-from pynput import keyboard
-import webbrowser
+from requests_html import HTMLSession # Importing a thing that lets me do website interaction stuff
+from pynput import keyboard # Importing a thing that lets me detect key presses
+import webbrowser # Importing a thing that lets me open websites in a web browser
 
-visited_links = []
-keyword = 'https://en.wikipedia.org/wiki'
-doCrawl = True
+visited_links = [] # Set up a list that I will add visited links to
+keyword = 'https://en.wikipedia.org/wiki/' # Set up a keyword that I will make sure is in each link (to make sure it doesn't leave wikipedia)
+doCrawl = True # Set up a variable that I will use to tell the crawler when to stop
 
-def on_press(key):
-    if key == keyboard.Key.esc:
-        global doCrawl 
-        doCrawl = False
-        return False
+def on_press(key): # Set up a function that will run when a key is pressed
+    if key == keyboard.Key.esc: # If the key pressed is escape
+        global doCrawl # Make sure we have the right variable
+        doCrawl = False # Set that variable to false
+        return False # End the keyboard listener
 
-def crawl(url,session):
-    r = session.get(url)
-    for link in r.html.absolute_links:
-        if doCrawl is False:
-            break
-        elif link not in visited_links and keyword in link:
-            visited_links.append(link)
-            print(link)
-            crawl(link,session)
+def crawl(url,session): # Set up a function that will run on each link
+    r = session.get(url) # Get the page from the link
+    for link in r.html.absolute_links: # For each link on the page
+        if doCrawl is False: # If I have told the crawler to stop
+            break # Stop the crawler
+        elif link not in visited_links and keyword in link: # If the link is not in the visited links list and the keyword is in the link
+            visited_links.append(link) # Add the link to the visited links list
+            print(link) # Print the link to the console
+            crawl(link,session) # Run this function again on the new found link (this is why it's called recursive)
 
-def main(url):
-    listener = keyboard.Listener(on_press=on_press)
-    listener.start()
-    #listener.join()
-    session = HTMLSession()
-    crawl(url,session)
-    webbrowser.open(visited_links[-1])
+def main(url): # Set up the main function that will run at the start of the program
+    listener = keyboard.Listener(on_press=on_press) # Set up the keyboard listener
+    listener.start() # Start the listener
+    session = HTMLSession() # Make the website interaction thing work
+    crawl(url,session) # Run the crawler on the url defined below
+    webbrowser.open(visited_links[-1]) # Once the crawling is done, open the last link in the visited links list in my web browser
 
-if __name__=='__main__':
-    url = 'https://en.wikipedia.org/wiki/Special:Random'
-    main(url)
+url = 'https://en.wikipedia.org/wiki/Special:Random' # Set a variable to the link I want the crawler to start on
+main(url) # Run the main function, sending it the link I want the crawler to start on
